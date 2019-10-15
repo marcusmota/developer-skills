08a55c8f33b95e10e6bf257b9fac8f9035e1824b
marcus mota <marcushcsm@gmail.com>
2019-10-15 21:14:59 +0100

    minor updates

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const app = require('./src/app')
+const app = require("./src/app")
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
-    console.log(`App running at port ${PORT}`);
+    console.log(`App running at port ${PORT} - env ${process.env.NODE_ENV} - ${new Date().toISOString()}`)
 })
