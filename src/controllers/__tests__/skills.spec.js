2d174f3995d9b47953df165154016519b1d20cdd
marcus mota <marcushcsm@gmail.com>
2019-10-15 20:39:28 +0100

    create the controllers and its tests

---
+++ src/controllers/__tests__/skills.spec.js
@@ -0,0 +1,102 @@
+const models = require("./../../models")
+const request = require("supertest")
+const app = require("../../app")
+const skillControler = require("../skills")
+
+const mockRequest = () => {}
+
+const mockResponse = () => {
+    const res = {}
+    res.status = jest.fn().mockReturnValue(res)
+    res.json = jest.fn().mockReturnValue(res)
+    return res
+}
+
+const storeHelper = async () => {
+    const javascript = await models.skills.create({
+        description: "Javascript"
+    })
+
+    const java = await models.skills.create({
+        description: "Java"
+    })
+
+    const dotnet = await models.skills.create({
+        description: "dotnet"
+    })
+
+    const developer = await models.developers.create({
+        name: "Marcus Mota"
+    })
+
+    const cloudoki = await models.companies.create({
+        name: "cloudoki"
+    })
+
+    await developer.setCompany(cloudoki)
+    await developer.setSkills([javascript])
+
+    const aws = await models.companies.create({
+        name: "aws"
+    })
+
+    const anotherDeveloper = await models.developers.create({
+        name: "Kimberly"
+    })
+
+    await anotherDeveloper.setCompany(aws)
+    await anotherDeveloper.setSkills([dotnet, java, javascript])
+}
+describe("developers controller tests", () => {
+    beforeEach(async () => {
+        models.developers.destroy({ where: {} })
+        models.companies.destroy({ where: {} })
+        models.skills.destroy({ where: {} })
+        /*
+        Seeder function to store the expected answer
+        */
+        await storeHelper()
+    })
+
+    describe("GET /fetch-skills-with-companies", () => {
+        it("should return 200 with the languages and the companies using it", async () => {
+            const result = await request(app)
+                .get(`/fetch-skills-with-companies`)
+                .set("Accept", /application\/json/)
+
+            const possibleCompanies = ["cloudoki", "aws"].sort()
+            const possibleSkills = ["Javascript", "Java", "dotnet"].sort()
+            const returnedSkills = result.body.map(skill => skill.language).sort()
+
+            result.body.forEach(sk => {
+                if (sk === "Javascript") {
+                    expect(sk.companies.sort()).toEqual(possibleCompanies)
+                } else if (sk === "Java") {
+                    expect(sk.companies.sort()).toEqual(["aws"])
+                } else if (sk === "dotnet") {
+                    expect(sk.companies.sort()).toEqual(["aws"])
+                }
+            })
+            expect(result.status).toBe(200)
+            expect(returnedSkills).toEqual(possibleSkills)
+        })
+        it("should return 422", async () => {
+            const res = mockResponse()
+            const req = mockRequest()
+            await skillControler.fetchSkillsWithCompaniesUsingIt(req, res)
+            console.log(res)
+            expect(res.status).toHaveBeenCalledWith(200)
+        })
+    })
+    describe("GET /fetch-skills-count", () => {
+        it("should return 200 with the languages and the count of developers using it", async () => {
+            const result = await request(app)
+                .get(`/fetch-skills-count`)
+                .set("Accept", /application\/json/)
+            expect(result.body["Java"]).toBe(1)
+            expect(result.body["Javascript"]).toBe(2)
+            expect(result.body["dotnet"]).toBe(1)
+            expect(Object.keys(result.body).length).toBe(3)
+        })
+    })
+})
